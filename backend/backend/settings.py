3a9a526dd1fead6ecf88f04fb81c975e5b9eccce
toastmanager <mrbonkyou@gmail.com>
2023-10-12 22:27:03 +0900

    1. Fixed Comments model 2. Changed view in admin panel 3. Added rest_framework to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'post',
     'djoser',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- backend/post/models.py
+++ backend/post/models.py
@@ -10,16 +10,26 @@ class Post(models.Model):
     likes = models.ManyToManyField(User, related_name="post_likes", blank=True)
     publish_date = models.DateTimeField(default=timezone.now)
 
+    def __str__(self) -> str:
+        return f"{self.author} | {self.publish_date}"
+
 class PostFile(models.Model):
-    file = models.FileField(upload_to="media/")
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="files")
+    file = models.FileField(upload_to="media/")
+
+    def __str__(self) -> str:
+        return f"{self.post}"
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name="comment_likes")
     text = models.CharField(max_length=2200)
     post_date = models.DateTimeField(auto_now_add=True)
     parent = models.ForeignKey('self' , null=True , blank=True , on_delete=models.CASCADE , related_name='replies')
+
+    def __str__(self) -> str:
+        return f"{self.post} | {self.author} | {self.post_date}"
 
     @property
     def children(self):
