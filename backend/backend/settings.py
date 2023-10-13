448d0aecd1082ad576f8411261e8b2a7eb887418
toastmanager <mrbonkyou@gmail.com>
2023-10-13 09:50:10 +0900

    Fixed Rest_framework auth stuff

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
