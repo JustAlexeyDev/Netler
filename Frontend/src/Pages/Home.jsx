ca681a897a83ac92b294c421171f936ca574acc9
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-10-13 11:10:05 +0900

    added axios

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,36 +1,26 @@
 
 import { useState, useEffect } from "react";
+import axios from "axios";
+import React from "react";
 
+const baseURL = 'http://127.0.0.1:8000/posts/?format=json';
 const Home = () => {
-  const [posts, setPosts] = useState([]);
 
-  const getApiData = async () => {
-    const response = await fetch(
-     "http://0.0.0.0:8000/posts/?format=json"
-    ).then((response) => response.json()).then(data => {
-      setPosts(data)
-    });
-    console.log(posts);
-    return response;
-  };
+  const [post, setPost] = useState(null);
+
   useEffect(() => {
-    getApiData();
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
   }, []);
+
+  if (!post) return null;
 
   return(
     <div className="Page">
       Посты
       <div>
-        {posts.length > 0 && (
-          <div>
-            {posts.map(post => {
-              <div>
-                Name
-                {post.description}
-              </div>
-            })}
-          </div>
-        )}        
+        {post.description}
       </div>
     </div>
   );
