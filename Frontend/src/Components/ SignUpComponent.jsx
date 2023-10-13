ad9f4087cf52d730b0a19333a8970e19106e3ba4
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-10-13 10:34:22 +0900

    fix

--- Frontend/src/Components/ SignUpComponent.jsx
+++ Frontend/src/Components/ SignUpComponent.jsx
@@ -6,11 +6,11 @@ const SignUpComponent = () => {
 
   const getApiData = async () => {
     const response = await fetch(
-     "http://127.0.0.1:8000/auth/users/?format=json"
+     "http://127.0.0.1:8000/posts/"
     ).then((response) => response.json()).then(data => {
       setUsers(data)
     });
-    console.log(response);
+    console.log(users);
     return response;
   };
   useEffect(() => {
@@ -22,9 +22,9 @@ const SignUpComponent = () => {
   //   const requestOptions = {
   //       method: 'POST',
   //       headers: { 'Content-Type': 'application/json' },
-  //       body: JSON.stringify({ title: 'React Hooks POST Request Example' })
+  //       body: JSON.stringify({ title: 'React Hooks POST Request Example',  })
   //   };
-  //   fetch('http://127.0.0.1:8000/auth/users/?format=json', requestOptions)
+  //   fetch('http://127.0.0.1:8000/auth/token/login/', requestOptions)
   //       .then(response => response.json())
   //       .then(data => setPostId(data.id));
   //   // empty dependency array means this effect will only run once (like componentDidMount in classes)
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -4,18 +4,7 @@ const Home = () => {
 
   return(
     <div className="Page">
-      <div className="">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, inventore. Voluptates dolorem eaque officia numquam aliquam exercitationem nisi dolor quasi neque, architecto aliquid assumenda temporibus ipsam reiciendis repudiandae, sit nihil?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, quia eos ratione molestias omnis quidem obcaecati, nihil aut ab, voluptatem ut quisquam! Odio eveniet, illo magni quis maxime ex vitae?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat aperiam deleniti odio dicta voluptate doloremque perferendis laudantium libero. Dignissimos pariatur sit recusandae. Inventore voluptates reprehenderit animi culpa praesentium eos similique.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt sapiente qui tempore magni culpa dolores nam at, facilis alias aliquid voluptate iusto quisquam, minima, voluptas quod modi molestiae quasi quos?
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati mollitia aliquam quisquam, autem repellendus laboriosam suscipit nihil quam earum? Delectus consequuntur animi non doloribus aliquid nulla perspiciatis alias aperiam dolorum.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem ipsa doloribus assumenda velit explicabo, tenetur possimus cupiditate sit, hic quisquam totam aliquid repellendus! Beatae culpa sequi, est explicabo esse labore?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae architecto distinctio est, provident perspiciatis, nesciunt sed illo ducimus ut consequatur consectetur, molestiae animi id dignissimos sunt totam eos unde!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam rerum reiciendis officiis nostrum nobis at illum quisquam dolor nesciunt asperiores ipsam magnam, exercitationem nisi maxime natus molestiae voluptate voluptatum eligendi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus autem, sequi beatae rerum dolorem ut nulla eligendi eveniet iure vitae! Voluptate dolor ad in numquam iure aliquam earum perspiciatis quaerat.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero enim voluptates provident ratione, pariatur nostrum nesciunt harum voluptatem rerum soluta. Voluptatum qui nesciunt et quisquam culpa, dolore iusto placeat nulla.        
-      </div>
+
     </div>
   );
 }
