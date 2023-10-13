018c1713b943d3f6d78f1972cdfa8d6cc54ccb4d
toastmanager <mrbonkyou@gmail.com>
2023-10-13 09:49:44 +0900

    Fixed CORS error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'backend.midleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
