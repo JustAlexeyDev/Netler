2967381e09fdba543c4043c801143545c7352eef
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-10-13 10:53:18 +0900

    Posts (Not Working)

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,10 +1,37 @@
+
 import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getApiData = async () => {
+    const response = await fetch(
+     "http://0.0.0.0:8000/posts/?format=json"
+    ).then((response) => response.json()).then(data => {
+      setPosts(data)
+    });
+    console.log(posts);
+    return response;
+  };
+  useEffect(() => {
+    getApiData();
+  }, []);
 
   return(
     <div className="Page">
-
+      Посты
+      <div>
+        {posts.length > 0 && (
+          <div>
+            {posts.map(post => {
+              <div>
+                Name
+                {post.description}
+              </div>
+            })}
+          </div>
+        )}        
+      </div>
     </div>
   );
 }
