9b97159bfc7e2a7964e7f1202e60715fadd2200a
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-10-13 15:57:47 +0900

    added imges maybe

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -4,30 +4,24 @@ import axios from "axios";
 import React from "react";
 // Vars
 const postsURL = 'http://127.0.0.1:8000/posts/?format=json';
-const postImageURL = 'http://127.0.0.1:8000/posts_files/?format=json';
 // Render
 const Home = () => {
   const [posts, setPosts] = useState(null);
-  const [images, setImages] = useState(null);
+  const [files, setFiles] = useState(null);
   useEffect(() => {
     axios.get(postsURL).then((response) => {
       setPosts(response.data);
+      setFiles(response.data);
     });
   }, []);
-  useEffect(() => {
-    axios.get(postImageURL).then((response) => {
-      setImages(response.data);
-    });
-  }, []);
-  console.log(images);
   console.log(posts);
   if (!posts) return null;
-  if (!images) return null;
+  if (!files) return null;
   // Page
   return(
     <div className="Page">
       <div>
-        {posts.length && images.length > 0 && (
+        {posts.length > 0 && (
           <div className="Post-Box">
             {posts.map(post => (
               <div className="Post-Container">
@@ -35,11 +29,15 @@ const Home = () => {
                   <span>Avatar: </span>
                   <span>Author: {post.author_name}</span>
                 </div>
-                {images.map(image => (
-                  <div className="Post-Image">
-                    <img src={image.file} alt="photo" />
+                {files.length > 0 && (
+                  <div>
+                    {files.map(file => (
+                      <div>
+                        <img src={file.file} />
+                      </div>
+                    ))}
                   </div>
-                ))}
+                )}
 
                 <div className="Post-Nav">
                   <span>Likes: {post.likes}</span>
