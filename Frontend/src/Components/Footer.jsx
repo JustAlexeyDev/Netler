3cb1824361f14522fbe836dcd0135056bc0840d3
toastmanager <mrbonkyou@gmail.com>
2023-10-15 06:55:07 +0900

    created base for getting authorized user data

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -5,8 +5,30 @@ import AddPost from '../Assets/Icons/addPostIcon.svg';
 import NotificationIcon from '../Assets/Icons/notificationIcon.svg';
 // Import React Libs
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 // import {Home, Users2, Plus, Bell} from 'lucide-react';
+const userDataURL = 'http://127.0.0.1:8000/get_user/';
 const Footer = () => {
+  const [userData, setUserData] = useState({})
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const token = localStorage.getItem('token')
+      if (token) {
+        const response = await axios.get(userDataURL, {
+          headers: {
+            Authorization: `Token ${localStorage.getItem('token')}`
+          }
+        });
+        setUserData(response.data);
+      } else {
+        console.log('Not authorized')
+      }
+    };
+    getUserData();
+  }, []);
+
   return(
     <div className="Footer">
       <div className='Footer_Container'>
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import User
 from .serializers import UserSerializer
@@ -6,3 +9,22 @@ from .serializers import UserSerializer
 class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+@api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+def get_user(request):
+    user = request.user
+    if user.avatar:
+        return Response({
+            'id': user.id,
+            'email': user.email,
+            'username': user.username,
+            # 'avatar': user.avatar,
+        })
+    else:
+        return Response({
+            'id': user.id,
+            'email': user.email,
+            'username': user.username,
+            'avatar': None,
+        })
