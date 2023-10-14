dfe3dc214a9e67f9fd010460404df8178a30cbd9
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-10-14 10:47:57 +0900

    added profile demo

--- Frontend/src/Pages/Profile.jsx
+++ Frontend/src/Pages/Profile.jsx
@@ -1,4 +1,14 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Profile = () => {
+ const userURL = 'http://127.0.0.1:8000/users/1'
+ const [userData, setUserData] = useState(null);
+ useEffect(() => {
+  axios.get(userURL).then((response) => {
+    setUserData(response.data);
+  });
+ }, []);
  return(
   <div className="Page">
    <div className="Profile-Banner">
