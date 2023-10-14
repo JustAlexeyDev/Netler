3549cb6e88acb5c93b50b275797b6bedd63bd876
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-10-14 09:18:10 +0900

    Footer fixed

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -1,17 +1,22 @@
-// // Import Icons
-// import HomeIcon from '../Assets/Icons/homeIcon.svg';
-// import FriendsIcon from '../Assets/Icons/friendsIcon.svg';
-// import AddPost from '../Assets/Icons/addPostIcon.svg';
-// import notificationIcon from '../Assets/Icons/notificationIcon.svg';
-// // Import React Libs
-// import { Link } from 'react-router-dom';
-
+// Import Icons
+import HomeIcon from '../Assets/Icons/homeIcon.svg';
+import FriendsIcon from '../Assets/Icons/friendsIcon.svg';
+import AddPost from '../Assets/Icons/addPostIcon.svg';
+import NotificationIcon from '../Assets/Icons/notificationIcon.svg';
+// Import React Libs
+import { Link } from 'react-router-dom';
+// import {Home, Users2, Plus, Bell} from 'lucide-react';
 const Footer = () => {
-
-
   return(
-    <div className="Footer_Container">
-      
+    <div className="Footer">
+      <div className='Footer_Container'>
+        <Link to='/Home'><img src={HomeIcon} alt='icon'/></Link>
+        <Link to='/'><img src={FriendsIcon} alt='icon'/></Link>
+        <Link to='/AddPost'><img src={AddPost} alt='icon'/></Link>
+        <Link to='/Notifications'><img src={NotificationIcon} alt='icon'/></Link>    
+        <Link to='/Profile'>Profile</Link>    
+      </div>
+
     </div>
   );
 }
