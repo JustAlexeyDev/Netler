30211879825b624ddd742df1c6dafd1659d1c60e
toastmanager <mrbonkyou@gmail.com>
2023-10-15 12:16:50 +0900

    Temporary avatar fix

--- backend/user/models.py
+++ backend/user/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='media/avatars/', blank=False)
-    REQUIRED_FIELDS = ["email", "password"]
+    REQUIRED_FIELDS = ["email", "password", "avatar"]
