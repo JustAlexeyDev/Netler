513522355858ef78127517ba90a95775778ac3ab
toastmanager <mrbonkyou@gmail.com>
2023-10-15 18:39:15 +0900

    Added banner

--- backend/user/models.py
+++ backend/user/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to='avatars/', blank=False)
+    avatar = models.ImageField(upload_to='avatars/', blank=True)
+    banner = models.ImageField(upload_to='banners/', blank=True)
     friends = models.ManyToManyField("User", blank=True, related_name="user_friends")
     subscriptions = models.ManyToManyField("User", blank=True, related_name="user_subscriptions")
     REQUIRED_FIELDS = ["email", "password", "avatar"]
