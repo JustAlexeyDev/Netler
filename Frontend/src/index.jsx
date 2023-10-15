45b3329f72da9a6b32de57956f983b9ba30dd1c9
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-10-16 03:00:18 +0900

    hotfin index.jsx

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -8,12 +8,12 @@ import Footer from "./Components/Footer";
 // Import Pages
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
-import SignUp from "./Pages/SignUp";
+import Auth from "./Pages/Auth";
 import Profile from "./Pages/Profile";
 import AddPost from "./Pages/AddPost";
 import Notifications from "./Pages/Notifications";
 import PeopleList from "./Pages/PeopleList";
-import SignIn from "./Pages/SignIn";
+import LoginIn from "./Pages/LoginIn";
 // Import Css
 import './Assets/Css/Reset.css';
 import './Assets/Css/Global.css';
@@ -24,14 +24,14 @@ ReactDOM.render(
       <Header/>
       <Routes>
         <Route path="/*" element={<NotFound />} />
-        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<LoginIn />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="/Auth" element={<Auth />} />
         <Route path="/Profile/:id" element={<Profile />} />
         <Route path="/AddPost" element={<AddPost />} />
         <Route path="/Notifications" element={<Notifications />} />
         <Route path="/PeopleList" element={<PeopleList />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/LoginIn" element={<LoginIn />} />
       </Routes>
       <Footer />
     </BrowserRouter>
