79974d7a968a67a9020eded25004a296f0638113
toastmanager <mrbonkyou@gmail.com>
2023-10-15 17:16:37 +0900

    Changed getting subscribers and friends by users/user_id/friends and users/user_id/subscribers

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -2,19 +2,7 @@ from rest_framework import serializers
 
 from .models import User
 
-class SubscriberSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "username", "avatar")
-
-class UserSerializer(serializers.ModelSerializer):
-    subscribers = serializers.SerializerMethodField()
-
-    def get_subscribers(self, obj):
-        queryset = User.objects.filter(subscriptions=obj.pk)
-        serializer = SubscriberSerializer(queryset, many=True)
-        return serializer.data
-
-    class Meta:
-        model = User
-        fields = ("id", "username", "avatar", "friends", "subscribers")
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -9,6 +9,18 @@ from .serializers import UserSerializer
 class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=True, methods=['GET'])
+    def subscribers(self, request, pk=None):
+        queryset = User.objects.filter(subscriptions=pk)
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['GET'])
+    def friends(self, request, pk=None):
+        queryset = User.objects.get(pk=pk).friends.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 @api_view(['GET'])
 # @permission_classes([IsAuthenticated])
