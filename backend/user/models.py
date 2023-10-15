89fa8b07e1a21ed2bedfa2f438b3905a1c582e90
toastmanager <mrbonkyou@gmail.com>
2023-10-15 16:55:43 +0900

    1. Added id in admin panel for posts 2. Moved avatars from media/media/avatars/ to media/avatars/ 3. Added friends and subscribers

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# from .serializers import UserSerializer
+
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to='media/avatars/', blank=False)
+    avatar = models.ImageField(upload_to='avatars/', blank=False)
+    friends = models.ManyToManyField("User", blank=True, related_name="user_friends")
+    subscriptions = models.ManyToManyField("User", blank=True, related_name="user_subscriptions")
     REQUIRED_FIELDS = ["email", "password", "avatar"]
+
+    # @property
+    # def subscribers(self):
+    #     queryset = User.objects.filter(subscriptions=self.pk)
+    #     serializer = UserSerializer(queryset, many=True)
+    #     return serializer.data
--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -2,7 +2,19 @@ from rest_framework import serializers
 
 from .models import User
 
-class UserSerializer(serializers.ModelSerializer):
+class SubscriberSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "username", "avatar")
+
+class UserSerializer(serializers.ModelSerializer):
+    subscribers = serializers.SerializerMethodField()
+
+    def get_subscribers(self, obj):
+        queryset = User.objects.filter(subscriptions=obj.pk)
+        serializer = SubscriberSerializer(queryset, many=True)
+        return serializer.data
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "avatar", "friends", "subscribers")
