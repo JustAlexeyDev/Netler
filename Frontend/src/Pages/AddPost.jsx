f6c3548b68603467a2840b9449f20d671283c695
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-10-16 12:02:57 +0900

    test

--- Frontend/src/Pages/AddPost.jsx
+++ Frontend/src/Pages/AddPost.jsx
@@ -4,18 +4,51 @@ import backendIP from '../vars'
 const postSendURL = `${backendIP}/create_post/`;
 
 const AddPost = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const [desc, setDesc] = useState('');
   const fileInputRef = useRef(null);
   const [selectedFiles, setSelectedFiles] = useState([]);
   const handleBrowseClick = () => {
     fileInputRef.current.click();
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!isSubmitting) {
+      setIsSubmitting(true);
+
+      e.preventDefault();
+      try {
+        const formData = new FormData();
+        selectedFiles.forEach((file) => {
+          formData.append('files', file);
+        });
+        formData.append('description', desc);
+  
+        const response = axios.post(postSendURL, formData, {
+          headers: {
+            Authorization: `Token ${localStorage.getItem('token')}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+  
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+        alert('Вы не авторизованы')
+      }
+
+      setTimeout(() => {
+        setIsSubmitting(false);
+      }, 3000); // Установите желаемое время задержки в миллисекундах
+    }
+  };
   const handleFileChange = (event) => {
     event.stopPropagation();
     const files = Array.from(event.target.files);
     setSelectedFiles(files);
   };
-  const sendPost = async (e) => {
+  const sendPost = (e) => {
     e.preventDefault();
     try {
       const formData = new FormData();
@@ -24,7 +57,7 @@ const AddPost = () => {
       });
       formData.append('description', desc);
 
-      const response = await axios.post(postSendURL, formData, {
+      const response = axios.post(postSendURL, formData, {
         headers: {
           Authorization: `Token ${localStorage.getItem('token')}`,
           'Content-Type': 'multipart/form-data',
@@ -39,7 +72,7 @@ const AddPost = () => {
   };
   return (
     <div className="Page">
-      <form onSubmit={sendPost} method="POST">
+      <form onSubmit={handleSubmit} method="POST">
         <div>
           <button onClick={handleBrowseClick}>Выбрать файлы</button>
           <input
@@ -61,7 +94,9 @@ const AddPost = () => {
           value={desc}
           onChange={(e) => setDesc(e.target.value)}
         />
-        <button type="submit">Отправить</button>
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? 'Отправка...' : 'Отправить'}
+        </button>
       </form>
     </div>
   );
