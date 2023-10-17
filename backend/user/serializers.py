29141b8a3e93a24ee9dcd0e541e67088e51c6519
toastmanager <mrbonkyou@gmail.com>
2023-10-17 09:02:38 +0900

    Added is verificated variable in user model

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -5,4 +5,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username", "avatar", "banner")
+        fields = ("id", "username", "avatar", "banner", "is_verificated")
